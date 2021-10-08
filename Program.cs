1119229d8b923cf7c9fb9a3c1338701ef9db0714
Minh <ntri4548@gmail.com>
2021-10-08 10:54:03 +0700

    Add QuickSort with Stack

--- Program.cs
+++ Program.cs
@@ -13,16 +13,31 @@ namespace Stack_Assignment
             // var stak = TaoChuoiKyTu(a);
             // Console.WriteLine(stak.ToArray());
 
-            Console.Write("Nhap mot cau bat ky: ");
+            // Console.Write("Nhap mot cau bat ky: ");
+            // string[] tk = Console.ReadLine().Split();
+            // string a = tk[0];
+            // var capacity = tk.Length;
+            // var x = TaoChuoiTu(tk);
+            // while(capacity != 0)
+            // {
+            //     var i = x.Pop();
+            //     Console.Write($"{i} ");
+            //     capacity--;
+            // }
+
+            Console.Write("Mang co bao nhieu phan tu?: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] data = new int[n];
             string[] tk = Console.ReadLine().Split();
-            string a = tk[0];
-            var capacity = tk.Length;
-            var x = TaoChuoiTu(tk);
-            while(capacity != 0)
+            for(int i = 0; i < n; i++)
             {
-                var i = x.Pop();
-                Console.Write($"{i} ");
-                capacity--;
+                data[i] = int.Parse(tk[i]);
+            }
+
+            QuickSortStack(ref data);
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write(data[i] + " ");
             }
         }
         static Stack<char> TaoChuoiKyTu(string a)
@@ -42,6 +57,63 @@ namespace Stack_Assignment
                 stak.Push(a[i]);
             }
             return stak;
+        }
+
+        public static void QuickSortStack(ref int[] data)
+        {
+            int startIndex = 0;
+            int endIndex = data.Length - 1;
+            int top = -1;
+            int[] stack = new int[data.Length];
+
+            stack[++top] = startIndex;
+            stack[++top] = endIndex;
+
+            while (top >= 0)
+            {
+                endIndex = stack[top--];
+                startIndex = stack[top--];
+
+                int p = Partition(ref data, startIndex, endIndex);
+
+                if (p - 1 > startIndex)
+                {
+                    stack[++top] = startIndex;
+                    stack[++top] = p - 1;
+                }
+
+                if (p + 1 < endIndex)
+                {
+                    stack[++top] = p + 1;
+                    stack[++top] = endIndex;
+                }
+            }
+        }
+
+        private static int Partition(ref int[] data, int left, int right)
+        {
+            int x = data[right];
+            int i = (left - 1);
+
+            for (int j = left; j <= right - 1; ++j)
+            {
+                if (data[j] <= x)
+                {
+                    ++i;
+                    Swap(ref data[i], ref data[j]);
+                }
+            }
+
+            Swap(ref data[i + 1], ref data[right]);
+
+            return (i + 1);
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
         }
     }
 }
